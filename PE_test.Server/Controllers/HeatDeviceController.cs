594ac67463a0bceb95e27a253ca4ae2bfe254916
valdas-k <valdas.kriunas@panko.lt>
2025-04-23 22:37:43 +0300

    Improved power functions

--- PE_test.Server/Controllers/HeatDeviceController.cs
+++ PE_test.Server/Controllers/HeatDeviceController.cs
@@ -1,199 +1,79 @@
-﻿using DocumentFormat.OpenXml.Spreadsheet;
-
-namespace PE_test.Server.Controllers;
+﻿namespace PE_test.Server.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
 public class HeatDeviceController : ControllerBase {
     private readonly ApplicationDbContext _context;
-    public HeatDeviceController(ApplicationDbContext context)
-    {
+    public HeatDeviceController(ApplicationDbContext context) {
         _context = context;
     }
 
-    public List<HeatDevice> heatDevices = [
-        new HeatDevice {ID = 1, Title = "AB Panevėžio stiklas", PowerCode = "94467", TempCode = 94462, P1Code = 94457, P2Code = 94458 },
-        new HeatDevice {ID = 2, Title = "UAB Biokuro energija", PowerCode = "69509", TempCode = 69361, P1Code = 69488, P2Code = 69489 },
-        new HeatDevice {ID = 3, Title = "PRK-1 GK Nr.6 ir Nr.7 kogeneracinis blokas", PowerCode = "41908-66239+49217+76713+76719-49209-49219", TempCode = 69361, P1Code = 69488, P2Code = 69489 },
-        new HeatDevice {ID = 4, Title = "PRK-1 VŠK Nr.8 ir Nr.9 blokas", PowerCode = "66239+49217+76713+76719", TempCode = 41905, P1Code = 41901, P2Code = 41902 },
-        new HeatDevice {ID = 5, Title = "PRK-1 VŠK Nr.5", PowerCode = "49209", TempCode = 41905, P1Code = 41901, P2Code = 41902 },
-        new HeatDevice {ID = 6, Title = "PEI katilinė VŠK Nr.1", PowerCode = "89652", TempCode = 71719, P1Code = 71717, P2Code = 71718 },
-        new HeatDevice {ID = 7, Title = "AB Panevėžio energija piko/rezervo įrenginiai", PowerCode = "89875-89652+49219", TempCode = 71719, P1Code = 71717, P2Code = 71718 },
-    ];
-
     [HttpGet]
     public async Task<List<HeatDevice>> GetStats() {
-        foreach (var device in heatDevices) {
+        //Kiekvienam sąrašo įrašui yra randamos reikšmės ir paleidžiama saugojimo funkcija
+        foreach (var device in HeatDevice.heatDevices) {
             device.Power = await GetPowerStats(device.PowerCode);
             device.TFlow = await GetTempStats(device.TempCode);
             (device.P01, device.P02) = await GetPressureStats(device.P1Code, device.P2Code);
         }
-        await SaveData(heatDevices);
-        return heatDevices;
-    }
-
-    [HttpGet("day")]
-    public async Task<List<HeatDevice>> GetDaysStats()
-    {
-        char[] chars = ['+', '-', '(', ')'];
-
-        decimal? power;
-        DateTime? time;
-
-        foreach (var device in heatDevices)
-        {
-            bool containsAnyChar = chars.Any(c => device.PowerCode.Contains(c));
-            if (containsAnyChar == false)
-            {
-                var query = from dcValue in _context.TblDcvalues
-                            join heatmeters in _context.CmpHeatMetersVals on dcValue.DevCompId equals heatmeters.DevCompId
-                            where dcValue.DcvalueId == Int32.Parse(device.PowerCode) &&
-                                heatmeters.RecordTime >= new DateTime(2024, 05, 01, 07, 00, 00) &&
-                                heatmeters.RecordTime <= new DateTime(2024, 05, 01, 08, 00, 00)
-                            select new
-                            {
-                                heatmeters.RecordTime,
-                                heatmeters.Power,
-                                dcValue.Caption,
-                                dcValue.UnitId
-                            };
-                var result = query.FirstOrDefault();
-
-                if (result == null)
-                {
-                    power = 0;
-                    time = null;
-                }
-                else
-                {
-                    power = result.Power;
-                    time = result.RecordTime;
-                    if (!result.Caption.Contains("mw", StringComparison.CurrentCultureIgnoreCase) || (result.UnitId != 56))
-                    {
-                        power /= 1000;
-                    }
-                    power = Math.Round(power.Value, 1);
-                }
-                Console.WriteLine(device.Title + ", " + time + ", " + power);
-            } else {
-                string[] strings = device.PowerCode.Split(chars);
-                int[] powerCodes = new int[strings.Length];
-                decimal[] powerValues = new decimal[strings.Length];
-
-                for (int i = 0; i < powerCodes.Length; i++)
-                {
-                    powerCodes[i] = int.Parse(strings[i]);
-                }
-
-                for (int i = 0; i < strings.Length; i++)
-                {
-                    var query = from dcValue in _context.TblDcvalues
-                                join heatmeters in _context.CmpHeatMetersVals on dcValue.DevCompId equals heatmeters.DevCompId
-                                where dcValue.DcvalueId == powerCodes[i] &&
-                                    heatmeters.RecordTime >= new DateTime(2024, 05, 01, 07, 00, 00) &&
-                                    heatmeters.RecordTime <= new DateTime(2024, 05, 01, 08, 00, 00)
-                                select new
-                                {
-                                    heatmeters.RecordTime,
-                                    heatmeters.Power,
-                                    dcValue.Caption,
-                                    dcValue.UnitId
-                                };
-                    var result = query.FirstOrDefault();
-                    if (result == null)
-                    {
-                        power = 0;
-                        time = null;
-                    }
-                    else
-                    {
-                        power = result.Power;
-                        time = result.RecordTime;
-                        if (!result.Caption.Contains("mw", StringComparison.CurrentCultureIgnoreCase) || (result.UnitId != 56))
-                        {
-                            power /= 1000;
-                        }
-                        power = Math.Round(power.Value, 1);
-                    }
-                    Console.WriteLine(device.Title + ", " + time + ", " + power);
-                }
-
-                string finalString = device.PowerCode;
-                for (int i = 0; i < strings.Length; i++)
-                {
-                    finalString = finalString.Replace(strings[i].ToString(), powerValues[i].ToString());
-                }
-
-                var answer = new DataTable().Compute(finalString, null);
-            }
-        }
-        return heatDevices;
+        await SaveData(HeatDevice.heatDevices);
+        return HeatDevice.heatDevices;
     }
 
     [HttpGet("power")]
-    public async Task<decimal?> GetPowerStats(string codeString) {
-        char[] chars = ['+', '-', '(', ')'];
+    public async Task<decimal?> GetPowerStats(string powerCodeString) {
+        //Tikrinami simboliai
+        char[] chars = ['+', '-'];
         decimal? power;
-        bool containsAnyChar = chars.Any(c => codeString.Contains(c));
-        if (containsAnyChar == false) {
-            int code = int.Parse(codeString);
-            var query = from dcValue in _context.TblDcvalues
+        bool containsChar = chars.Any(c => powerCodeString.Contains(c));
+
+        if (containsChar == false) {
+            //Jei nėra jokio papildomo simbolio, yra gaunama galios reikšmė
+            power = LastPowerQuery(powerCodeString);
+            return power;
+
+        } else {
+            //Jei yra papildomas simbolis, yra randami visi atskiri kodai
+            string[] strings = powerCodeString.Split(chars);
+            decimal[] powerValues = new decimal[strings.Length];
+            string finalString = powerCodeString;
+
+            for (int i = 0; i < strings.Length; i++) {
+                //Kiekvienam kodui yra gaunama galios reikšmė
+                powerValues[i] = LastPowerQuery(strings[i]).GetValueOrDefault();
+            }
+
+            for (int i = 0; i < strings.Length; i++) {
+                //Į originalią formulę yra įterpiamos galios reikšmės vieotje kodų
+                finalString = finalString.Replace(strings[i], powerValues[i].ToString());
+            }
+
+            //Atliekama funkcija, kuri tekstą paverčia matematine formule ir apskaičiuoja bendrą galios reikšmę
+            power = Convert.ToDecimal(new DataTable().Compute(finalString, null));
+            return power;
+        }
+    }
+
+    [HttpGet("power-query")]
+    public decimal? LastPowerQuery(string powerCodeString) {
+        //Vykdoma užklausa
+        decimal? power;
+        var query = from dcValue in _context.TblDcvalues
                     join heatMetersLast in _context.CmpHeatMetersValLasts on dcValue.DevCompId equals heatMetersLast.DevCompId
-                    where dcValue.DcvalueId == code
+                    where dcValue.DcvalueId == int.Parse(powerCodeString)
                     select new {
                         heatMetersLast.Power,
                         dcValue.Caption,
                         dcValue.UnitId
                     };
-            var result = query.FirstOrDefault();
-            power = result.Power;
-            if (power != null) {
-                if (!result.Caption.Contains("mw", StringComparison.CurrentCultureIgnoreCase) || (result.UnitId != 56)) {
-                    power /= 1000;
-                }
-                power = Math.Round(power.Value, 1);
-            }
-            return power;
-        } else {
-            string[] strings = codeString.Split(chars);
-            int[] powerCodes = new int[strings.Length];
-            decimal[] powerValues = new decimal[strings.Length];
 
-            for (int i = 0; i < powerCodes.Length; i++) {
-                powerCodes[i] = int.Parse(strings[i]);
-            }
-
-            for (int i = 0; i < strings.Length; i++) {
-                var query = from dcValue in _context.TblDcvalues
-                            join heatMetersLast in _context.CmpHeatMetersValLasts on dcValue.DevCompId equals heatMetersLast.DevCompId
-                            where dcValue.DcvalueId == powerCodes[i]
-                            select new {
-                                heatMetersLast.Power,
-                                dcValue.Caption,
-                                dcValue.UnitId
-                            };
-                var result = query.FirstOrDefault();
-                power = result.Power;
-                if (power != null) {
-                    if (!result.Caption.Contains("mw", StringComparison.CurrentCultureIgnoreCase) || (result.UnitId != 56)) {
-                        power /= 1000;
-                    }
-                    power = Math.Round(power.Value, 1);
-                    powerValues[i] = (decimal)power;
-                } else {
-                    powerValues[i] = 0;
-                }
-            }
-
-            string finalString = codeString;
-            for (int i = 0; i < strings.Length; i++) {
-                finalString = finalString.Replace(strings[i].ToString(), powerValues[i].ToString());
-            }
-
-            var answer = new DataTable().Compute(finalString, null);
-            decimal finalPower = Convert.ToDecimal(answer);
-            return finalPower;
+        //Gaunamas rezultatas, kuris yra patikrinamas ir grąžinamas
+        var result = query.FirstOrDefault();
+        power = result.Power;
+        if (!result.Caption.Contains("mw", StringComparison.CurrentCultureIgnoreCase) && (result.UnitId != 56)) {
+            power /= 1000;
         }
+        return power;
     }
 
     [HttpGet("tflow")]
@@ -321,4 +201,106 @@ public class HeatDeviceController : ControllerBase {
         memoryStream.Position = 0;
         return File(memoryStream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", fileName);
     }
+
+    [HttpGet("day")]
+    public async Task<List<HeatDevice>> GetDaysStats()
+    {
+        char[] chars = ['+', '-', '(', ')'];
+
+        decimal? power;
+        DateTime? time;
+
+        foreach (var device in HeatDevice.heatDevices)
+        {
+            bool containsAnyChar = chars.Any(c => device.PowerCode.Contains(c));
+            if (containsAnyChar == false)
+            {
+                var query = from dcValue in _context.TblDcvalues
+                            join heatmeters in _context.CmpHeatMetersVals on dcValue.DevCompId equals heatmeters.DevCompId
+                            where dcValue.DcvalueId == Int32.Parse(device.PowerCode) &&
+                                heatmeters.RecordTime >= new DateTime(2024, 05, 01, 07, 00, 00) &&
+                                heatmeters.RecordTime <= new DateTime(2024, 05, 01, 08, 00, 00)
+                            select new
+                            {
+                                heatmeters.RecordTime,
+                                heatmeters.Power,
+                                dcValue.Caption,
+                                dcValue.UnitId
+                            };
+                var result = query.FirstOrDefault();
+
+                if (result == null)
+                {
+                    power = 0;
+                    time = null;
+                }
+                else
+                {
+                    power = result.Power;
+                    time = result.RecordTime;
+                    if (!result.Caption.Contains("mw", StringComparison.CurrentCultureIgnoreCase) || (result.UnitId != 56))
+                    {
+                        power /= 1000;
+                    }
+                    power = Math.Round(power.Value, 1);
+                }
+                Console.WriteLine(device.Title + ", " + time + ", " + power);
+            }
+            else
+            {
+                string[] strings = device.PowerCode.Split(chars);
+                int[] powerCodes = new int[strings.Length];
+                decimal[] powerValues = new decimal[strings.Length];
+
+                for (int i = 0; i < powerCodes.Length; i++)
+                {
+                    powerCodes[i] = int.Parse(strings[i]);
+                }
+
+                for (int i = 0; i < strings.Length; i++)
+                {
+                    var query = from dcValue in _context.TblDcvalues
+                                join heatmeters in _context.CmpHeatMetersVals on dcValue.DevCompId equals heatmeters.DevCompId
+                                where dcValue.DcvalueId == powerCodes[i] &&
+                                    heatmeters.RecordTime >= new DateTime(2024, 05, 01, 07, 00, 00) &&
+                                    heatmeters.RecordTime <= new DateTime(2024, 05, 01, 08, 00, 00)
+                                select new
+                                {
+                                    heatmeters.RecordTime,
+                                    heatmeters.Power,
+                                    dcValue.Caption,
+                                    dcValue.UnitId
+                                };
+                    var result = query.FirstOrDefault();
+                    if (result == null)
+                    {
+                        power = 0;
+                        time = null;
+                    }
+                    else
+                    {
+                        power = result.Power;
+                        time = result.RecordTime;
+                        if (!result.Caption.Contains("mw", StringComparison.CurrentCultureIgnoreCase) || (result.UnitId != 56))
+                        {
+                            power /= 1000;
+                        }
+                        power = Math.Round(power.Value, 1);
+                    }
+                    Console.WriteLine(device.Title + ", " + time + ", " + power);
+                }
+
+                string finalString = device.PowerCode;
+                for (int i = 0; i < strings.Length; i++)
+                {
+                    finalString = finalString.Replace(strings[i].ToString(), powerValues[i].ToString());
+                }
+
+                var answer = new DataTable().Compute(finalString, null);
+            }
+        }
+        return HeatDevice.heatDevices;
+    }
+
+
 }
--- PE_test.Server/Global.cs
+++ PE_test.Server/Global.cs
@@ -4,9 +4,5 @@ global using ClosedXML.Excel;
 global using PE_test.Server.Data;
 global using PE_test.Server.Models;
 global using System.Data;
-global using System.Threading.Tasks;
-global using System;
-global using System.Collections.Generic;
-global using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace PE_test.Server;
