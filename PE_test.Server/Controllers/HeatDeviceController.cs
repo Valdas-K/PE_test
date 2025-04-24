898b8fc847f1d3a5466902278220acb641f458c9
valdas-k <valdas.kriunas@panko.lt>
2025-04-24 10:25:26 +0300

    Improved task 2

--- PE_test.Server/Controllers/HeatDeviceController.cs
+++ PE_test.Server/Controllers/HeatDeviceController.cs
@@ -54,10 +54,9 @@ public class HeatDeviceController : ControllerBase {
         }
     }
 
-    [HttpGet("power-query")]
+    [HttpGet("last-power-query")]
     public decimal? LastPowerQuery(string powerCodeString) {
         //Vykdoma užklausa
-        decimal? power;
         var query = from dcValue in _context.TblDcvalues
                     join heatMetersLast in _context.CmpHeatMetersValLasts on dcValue.DevCompId equals heatMetersLast.DevCompId
                     where dcValue.DcvalueId == int.Parse(powerCodeString)
@@ -68,10 +67,13 @@ public class HeatDeviceController : ControllerBase {
                     };
 
         //Gaunamas rezultatas, kuris yra patikrinamas ir grąžinamas
+        decimal? power = null;
         var result = query.FirstOrDefault();
-        power = result.Power;
-        if (!result.Caption.Contains("mw", StringComparison.CurrentCultureIgnoreCase) && (result.UnitId != 56)) {
-            power /= 1000;
+        if (result != null) {
+            power = result.Power;
+            if (!result.Caption.Contains("mw", StringComparison.CurrentCultureIgnoreCase) && (result.UnitId != 56)) {
+                power /= 1000;
+            }
         }
         return power;
     }
@@ -191,7 +193,6 @@ public class HeatDeviceController : ControllerBase {
             allCells.NumberFormat.Format = "0.0";
             worksheet.Range(1, 1, 5, index - 1).Style.Border.OutsideBorder = XLBorderStyleValues.Hair;
             worksheet.Range(1, 1, 5, index - 1).Style.Border.InsideBorder = XLBorderStyleValues.Hair;
-            worksheet.Range(1, 1, 5, index - 1).Style.Alignment.Vertical = XLAlignmentVerticalValues.Center;
             worksheet.Range(1, 1, 2, index - 1).Style.Font.Bold = true;
             worksheet.Columns().AdjustToContents(9, 25.0);
 
@@ -202,105 +203,140 @@ public class HeatDeviceController : ControllerBase {
         return File(memoryStream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", fileName);
     }
 
+
+    //Antra užduotis
     [HttpGet("day")]
-    public async Task<List<HeatDevice>> GetDaysStats()
-    {
-        char[] chars = ['+', '-', '(', ')'];
-
-        decimal? power;
-        DateTime? time;
-
-        foreach (var device in HeatDevice.heatDevices)
-        {
-            bool containsAnyChar = chars.Any(c => device.PowerCode.Contains(c));
-            if (containsAnyChar == false)
-            {
-                var query = from dcValue in _context.TblDcvalues
-                            join heatmeters in _context.CmpHeatMetersVals on dcValue.DevCompId equals heatmeters.DevCompId
-                            where dcValue.DcvalueId == Int32.Parse(device.PowerCode) &&
-                                heatmeters.RecordTime >= new DateTime(2024, 05, 01, 07, 00, 00) &&
-                                heatmeters.RecordTime <= new DateTime(2024, 05, 01, 08, 00, 00)
-                            select new
-                            {
-                                heatmeters.RecordTime,
-                                heatmeters.Power,
-                                dcValue.Caption,
-                                dcValue.UnitId
-                            };
-                var result = query.FirstOrDefault();
-
-                if (result == null)
-                {
-                    power = 0;
-                    time = null;
-                }
-                else
-                {
-                    power = result.Power;
-                    time = result.RecordTime;
-                    if (!result.Caption.Contains("mw", StringComparison.CurrentCultureIgnoreCase) || (result.UnitId != 56))
-                    {
-                        power /= 1000;
-                    }
-                    power = Math.Round(power.Value, 1);
-                }
-                Console.WriteLine(device.Title + ", " + time + ", " + power);
-            }
-            else
-            {
-                string[] strings = device.PowerCode.Split(chars);
-                int[] powerCodes = new int[strings.Length];
-                decimal[] powerValues = new decimal[strings.Length];
-
-                for (int i = 0; i < powerCodes.Length; i++)
-                {
-                    powerCodes[i] = int.Parse(strings[i]);
-                }
-
-                for (int i = 0; i < strings.Length; i++)
-                {
-                    var query = from dcValue in _context.TblDcvalues
-                                join heatmeters in _context.CmpHeatMetersVals on dcValue.DevCompId equals heatmeters.DevCompId
-                                where dcValue.DcvalueId == powerCodes[i] &&
-                                    heatmeters.RecordTime >= new DateTime(2024, 05, 01, 07, 00, 00) &&
-                                    heatmeters.RecordTime <= new DateTime(2024, 05, 01, 08, 00, 00)
-                                select new
-                                {
-                                    heatmeters.RecordTime,
-                                    heatmeters.Power,
-                                    dcValue.Caption,
-                                    dcValue.UnitId
-                                };
-                    var result = query.FirstOrDefault();
-                    if (result == null)
-                    {
-                        power = 0;
-                        time = null;
-                    }
-                    else
-                    {
-                        power = result.Power;
-                        time = result.RecordTime;
-                        if (!result.Caption.Contains("mw", StringComparison.CurrentCultureIgnoreCase) || (result.UnitId != 56))
-                        {
-                            power /= 1000;
-                        }
-                        power = Math.Round(power.Value, 1);
-                    }
-                    Console.WriteLine(device.Title + ", " + time + ", " + power);
-                }
-
-                string finalString = device.PowerCode;
-                for (int i = 0; i < strings.Length; i++)
-                {
-                    finalString = finalString.Replace(strings[i].ToString(), powerValues[i].ToString());
-                }
-
-                var answer = new DataTable().Compute(finalString, null);
-            }
+    public async Task<List<HeatDevice>> GetDayStats() {
+        //Kiekvienam sąrašo įrašui yra randamos galios reikšmės ir paleidžiama saugojimo funkcija
+        foreach (var device in HeatDevice.heatDevices) {
+            device.Power = await GetDaysPower(device.PowerCode);
         }
+        await SaveDayData(HeatDevice.heatDevices);
         return HeatDevice.heatDevices;
     }
 
+    [HttpGet("day-power-query")]
+    public decimal? DayPowerQuery(string powerCodeString) {
+        //Vykdoma užklausa
+        var query = from dcValue in _context.TblDcvalues
+                    join heatmeters in _context.CmpHeatMetersVals on dcValue.DevCompId equals heatmeters.DevCompId
+                    where dcValue.DcvalueId == int.Parse(powerCodeString) &&
+                        heatmeters.RecordTime >= new DateTime(2024, 05, 01, 07, 00, 00) &&
+                        heatmeters.RecordTime <= new DateTime(2024, 05, 01, 08, 00, 00)
+                    select new {
+                        heatmeters.RecordTime,
+                        heatmeters.Power,
+                        dcValue.Caption,
+                        dcValue.UnitId
+                    };
 
+        //Gaunamas rezultatas, kuris yra patikrinamas ir grąžinamas
+        decimal? power = null;
+        var result = query.FirstOrDefault();
+        if (result != null) {
+            power = result.Power;
+            if (!result.Caption.Contains("mw", StringComparison.CurrentCultureIgnoreCase) && (result.UnitId != 56)) {
+                power /= 1000;
+            }
+        }
+
+        return power;
+    }
+
+    [HttpGet("day-power")]
+    public async Task<decimal?> GetDaysPower(string powerCode) {
+        //Tikrinami simboliai
+        char[] chars = ['+', '-'];
+        decimal? power;
+        bool containsChar = chars.Any(c => powerCode.Contains(c));
+        if (containsChar == false) {
+            //Jei nėra jokio papildomo simbolio, yra gaunama galios reikšmė
+            power = DayPowerQuery(powerCode);
+        } else {
+            //Jei yra papildomas simbolis, yra randami visi atskiri kodai
+            string[] strings = powerCode.Split(chars);
+            decimal[] powerValues = new decimal[strings.Length];
+            string finalString = powerCode;
+
+            //Kiekvienam kodui yra gaunama galios reikšmė
+            for (int i = 0; i < strings.Length; i++) {
+                powerValues[i] = DayPowerQuery(strings[i]).GetValueOrDefault();
+            }
+
+            //Į originalią formulę yra įterpiamos galios reikšmės vieotje kodų
+            for (int i = 0; i < strings.Length; i++) {
+                finalString = finalString.Replace(strings[i], powerValues[i].ToString());
+            }
+
+            //Atliekama funkcija, kuri tekstą paverčia matematine formule ir apskaičiuoja bendrą galios reikšmę 
+            power = Convert.ToDecimal(new DataTable().Compute(finalString, null));
+        }
+        return power;
+    }
+
+    [HttpGet("save-day-data")]
+    public async Task<ActionResult> SaveDayData(List<HeatDevice> devices) {
+        //Sukuriamas Excel failas ir failo pavadinimas
+        var memoryStream = new MemoryStream();
+        string fileName = "devices_1.xlsx";
+        using (var workbook = new XLWorkbook()) {
+            //Sukuriamas darbo lapas
+            var worksheet = workbook.Worksheets.Add("Pirmas");
+
+            //Sujungiami informacijos lengeliai
+            worksheet.Cell(1, 1).Value = "Data";
+            worksheet.Range("A1:A3").Merge();
+            worksheet.Cell(1, 2).Value = "Laikas";
+            worksheet.Range("B1:C3").Merge();
+
+            worksheet.Cell(4, 1).Value = "2024-05-01";
+            worksheet.Cell(4, 2).Value = "07-08";
+            worksheet.Cell(4, 3).Value = "1";
+
+            //Šilumos gamintojų tipai ir sujungiami langeliai
+            worksheet.Cell(1, 4).Value = "AŠG";
+            worksheet.Cell(1, 5).Value = "NŠG";
+            worksheet.Cell(1, 6).Value = "AB Panevėžio energija konkurenciniai įrenginiai";
+            worksheet.Range("F1:I1").Merge();
+
+            //Katilinių pavadinimai ir sujungiami langeliai
+            worksheet.Cell(2, 4).Value = "AB Panevėžio stiklas";
+            worksheet.Cell(2, 5).Value = "UAB Biokuro energija";
+            worksheet.Cell(2, 6).Value = "PRK-1 GK Nr.6 ir Nr.7 kogeneracinis blokas";
+            worksheet.Cell(2, 7).Value = "PRK-1 VŠK Nr.8 ir Nr.9 blokas";
+            worksheet.Cell(2, 8).Value = "PEI katilinė VŠK Nr.1";
+            worksheet.Cell(2, 9).Value = "PRK-1 VŠK Nr.5";
+            worksheet.Cell(1, 10).Value = "AB Panevėžio energija piko/rezervo įrenginiai";
+            worksheet.Range("J1:J2").Merge();
+
+            //Įdedami duomenys ir kita pasikartojanti informacija
+            //index - stulpelio numeris
+            int index = 4;
+            foreach (var device in devices)
+            {
+                //Galia Power
+                if (device.Power == null)
+                    worksheet.Cell(4, index).Value = "-";
+                else
+                    worksheet.Cell(4, index).Value = device.Power;
+                worksheet.Cell(3, index).Value = "Šilumos gamyba (Mw)";
+                index++;
+            }
+            //Pridedami lentelės stiliai: linijų plotis, teksto lygiavimas, numerių formatas, pastorintas šriftas ir automatiniai langelių pločiai
+            var allCells = worksheet.RangeUsed().Style;
+            allCells.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+            allCells.Alignment.Vertical = XLAlignmentVerticalValues.Top;
+            allCells.NumberFormat.Format = "0.0";
+            worksheet.Range(1, 1, 4, index - 1).Style.Border.OutsideBorder = XLBorderStyleValues.Hair;
+            worksheet.Range(1, 1, 4, index - 1).Style.Border.InsideBorder = XLBorderStyleValues.Hair;
+            worksheet.Range(1, 1, 2, index - 1).Style.Font.Bold = true;
+            worksheet.Range(1, 1, 5, index - 1).Style.Alignment.WrapText = true;
+            worksheet.Columns().AdjustToContents(10.0, 25.0);
+
+            //Išsaugomas failas
+            workbook.SaveAs(fileName);
+        }
+        memoryStream.Position = 0;
+        return File(memoryStream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", fileName);
+    }
 }
--- PE_test.Server/Data/HeatDevice.cs
+++ PE_test.Server/Data/HeatDevice.cs
@@ -17,8 +17,8 @@ public class HeatDevice {
         new HeatDevice { Title = "UAB Biokuro energija", PowerCode = "69509", TempCode = 69361, P1Code = 69488, P2Code = 69489 },
         new HeatDevice { Title = "PRK-1 GK Nr.6 ir Nr.7 kogeneracinis blokas", PowerCode = "41908-66239+49217+76713+76719-49209-49219", TempCode = 69361, P1Code = 69488, P2Code = 69489 },
         new HeatDevice { Title = "PRK-1 VŠK Nr.8 ir Nr.9 blokas", PowerCode = "66239+49217+76713+76719", TempCode = 41905, P1Code = 41901, P2Code = 41902 },
-        new HeatDevice { Title = "PRK-1 VŠK Nr.5", PowerCode = "49209", TempCode = 41905, P1Code = 41901, P2Code = 41902 },
         new HeatDevice { Title = "PEI katilinė VŠK Nr.1", PowerCode = "89652", TempCode = 71719, P1Code = 71717, P2Code = 71718 },
+        new HeatDevice { Title = "PRK-1 VŠK Nr.5", PowerCode = "49209", TempCode = 41905, P1Code = 41901, P2Code = 41902 },
         new HeatDevice { Title = "AB Panevėžio energija piko/rezervo įrenginiai", PowerCode = "89875-89652+49219", TempCode = 71719, P1Code = 71717, P2Code = 71718 },
     ];
 }
